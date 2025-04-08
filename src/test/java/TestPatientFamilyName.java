da30cda9fa40d550ad051ad8fc620650ad0ff051
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-04-08 09:49:25 +0100

    Setting up test for patient family name

---
+++ src/test/java/TestPatientFamilyName.java
@@ -0,0 +1,20 @@
+import ie.atu.week12.Patient;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions;
+
+public class TestPatientFamilyName
+{
+        @Test
+        void createFamilyName()
+        {
+                Patient p = new Patient("James");
+                assertEquals("James"p.getTitle());
+        }
+
+        @Test
+        void invalidFamilyName()
+        {
+                Exception ex = assertThrows(IllegalArgumentException.class, () -> new Patient("Bob"));
+                assertEquals("Error", ex.getMessage());
+        }
+}
