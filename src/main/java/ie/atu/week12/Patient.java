c3114987ad7c4006653d923fccccb8a43864d9c8
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-04-08 09:15:13 +0100

    Created new project 'Patient' for OOP exam

---
+++ src/main/java/ie/atu/week12/Patient.java
@@ -0,0 +1,14 @@
+package ie.atu.week12;
+
+public class Patient
+{
+    private final String familyName;
+    private final String name;
+    private final String HSEnum;
+    private final int age;
+
+    public Patient(String familyName, String name, String HSEnum, int age)
+    {
+
+    }
+}
