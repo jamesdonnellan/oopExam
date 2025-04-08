5d6287046dddf8a49c9de923b3c1112e2b5e9557
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-04-08 09:24:30 +0100

    Setting up patient class + test class

--- src/main/java/ie/atu/week12/Patient.java
+++ src/main/java/ie/atu/week12/Patient.java
@@ -2,13 +2,56 @@ package ie.atu.week12;
 
 public class Patient
 {
-    private final String familyName;
-    private final String name;
-    private final String HSEnum;
-    private final int age;
+    private String familyName;
+    private String name;
+    private String HSEnum;
+    private int age;
+
 
     public Patient(String familyName, String name, String HSEnum, int age)
     {
+    this.familyName = familyName;
+    this.name = name;
+    this.HSEnum = HSEnum;
+    this.age = age;
+    }
+    public String getFamilyName()
+    {
+        return familyName;
+    }
 
+    public void setFamilyName(String familyName)
+    {
+        this.familyName = familyName;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getHSEnum()
+    {
+        return HSEnum;
+    }
+
+    public void setHSEnum(String HSEnum)
+    {
+        this.HSEnum = HSEnum;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
     }
 }
